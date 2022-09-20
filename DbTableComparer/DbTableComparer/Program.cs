33b8b309a24bb6edd0086387e9225f093814db8f
ony19161 <aman.19161@gmail.com>
2022-09-21 01:39:15 +0600

    argument validation added

--- DbTableComparer/DbTableComparer/Program.cs
+++ DbTableComparer/DbTableComparer/Program.cs
@@ -10,6 +10,46 @@ namespace DbTableComparer
     {
         static void Main(string[] args)
         {
+            bool willStartComparison = true;
+            var argValues = string.Join(" ", args).Split('-').Where(a => !string.IsNullOrEmpty(a)).ToList();
+
+            if (argValues.Count() < 3)
+            {
+                willStartComparison = false;
+                Console.WriteLine("Missing argument");
+            }
+
+            try
+            {
+                string firstTableName = argValues[0].Split(' ')[1];
+                string secondTableName = argValues[1].Split(' ')[1];
+                string pkName = argValues[2].Split(' ')[1];
+
+                if (string.IsNullOrWhiteSpace(firstTableName) ||
+                    string.IsNullOrWhiteSpace(secondTableName) ||
+                    string.IsNullOrWhiteSpace(pkName))
+                {
+                    willStartComparison = false;
+                    Console.WriteLine("Missing argument value");
+                }
+            }
+            catch
+            {
+                willStartComparison = false;
+                Console.WriteLine("Missing argument value");
+            }
+
+            if (willStartComparison)
+            {
+                Console.WriteLine("Start comparison");
+            }
+            else
+            {
+                Console.WriteLine("Operation aborted");
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
